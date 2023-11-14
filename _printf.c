e98b0d999f932182e357cf68b38ebfd4b6502d61
Thabani Takwena <thabanitakwena2.0@gmail.com>
2023-11-14 08:17:26 +0200

    Fixed gnu89 standard formatting

--- _printf.c
+++ _printf.c
@@ -10,18 +10,14 @@
  */
 int _printf(const char *format, ...)
 {
+    int i = 0;
+    int count = 0;
+    va_list args;
+
     if (format == NULL)
     {
         return -1;
     }
-
-    int i;
-    int count;
-
-    i = 0;
-    count = 0;
-    
-    va_list args;
 
     va_start(args, format);
 
